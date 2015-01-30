f23f47a9ec5ab08bfb8bfd578b3a551f29f76e3c
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2015-01-30 15:49:34 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#!usr/env/bin/python2.7
+
+import __init__
+import sqlite3
+
+var course = raw_input("Enter the course name.")
+for x in c.execute("SELECT name from course")
+	if x == course
+if(course exist):
+
+else:
+	var strength=int(raw_input("Enter the strength of student"))
+	
+	
