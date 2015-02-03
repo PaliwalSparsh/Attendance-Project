d4da4fd9238d6851e7b74d60d34013ddc2449bf8
ABHAY RAIZADA <abhsag24@gmail.com>
2015-02-03 23:15:07 +0530

    corrected all execute parameters to tuple, selecting using fetchone() from cursor object, first working copy completed

--- __main__.py
+++ __main__.py
@@ -8,15 +8,15 @@ import func
 def main():
 	in_course = raw_input("Enter the course name.\n")
 	if func.course_exist(in_course,checkdb.c) :
-		strength = checkdb.c.execute("SELECT strength FROM courses where name=?;",in_course);
-		func.take_attendace(in_course)
+		checkdb.c.execute('SELECT strength FROM courses where name=?',(in_course,))
+		strength = checkdb.c.fetchone()
+		func.take_attendance(strength[0])
 
 	else :
-		in_strength = raw_input("Ohh..New Course..Enter the strength of class\n")
-		db_entry=[in_course,in_strength]
-		checkdb.c.execute('INSERT INTO courses VALUES(?,?)',db_entry)
+		in_strength = int(raw_input("Ohh..New Course..Enter the strength of class\n"))
+		checkdb.c.execute('INSERT INTO courses VALUES(?,?)',(in_course,in_strength))
 		func.create_course(in_course,in_strength,checkdb.c)
-		func.take_attendance(in_course)
+		func.take_attendance(in_strength)
 
 
 if __name__ == "__main__" : main()
