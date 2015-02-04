24449966104b6aec93aa19918b62f6bff7823c7a
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2015-02-04 12:45:38 +0530

    added the date coloumn to register

--- func.py
+++ func.py
@@ -1,13 +1,19 @@
 #!usr/env/bin/python2.7
 
 import sqlite3
+import datetime
 
-def take_attendance(strength):
+def take_attendance(strength,course,conn_obj): 
+	today = str(datetime.date.today())
+	conn_obj.execute("ALTER TABLE %s ADD %s char" %(course,today))  
 	for x in range(1,strength):
+		print ("%d ." %x)
 		if (raw_input("").lower())=='p':
 			print "present"
+			conn_obj.execute("UPDATE %s SET %s='P' WHERE roll=%d" %(course,today,x))
 		if (raw_input("").lower())=='a':
 			print "absent"
+			conn_obj.execute("UPDATE %s SET %s='A' WHERE roll=%d" %(course,today,x))
 
 def course_exist(course,conn_obj):
 	for x in conn_obj.execute("SELECT name from courses;"):
@@ -19,5 +25,6 @@ def create_course(course,strength,conn_obj):
 	conn_obj.execute('CREATE TABLE %s(roll int);' %course)
 	for x in range(1,strength):
 		conn_obj.execute("INSERT INTO %s VALUES(%d);" %(course,x))
+		
 
 if __name__ == "__main__":main()
