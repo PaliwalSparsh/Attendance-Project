bd438755c473f6414348a3c168b9090b25548f82
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2015-02-04 15:57:06 +0530

    Final working copy learned: try except ,pass by assignment

--- __main__.py
+++ __main__.py
@@ -10,13 +10,14 @@ def main():
 	if func.course_exist(in_course,checkdb.c) :
 		checkdb.c.execute('SELECT strength FROM courses where name=?',(in_course,))
 		strength = checkdb.c.fetchone()
-		func.take_attendance(strength[0])
+		func.take_attendance(strength[0],in_course,checkdb.c)
 
 	else :
 		in_strength = int(raw_input("Ohh..New Course..Enter the strength of class\n"))
 		checkdb.c.execute('INSERT INTO courses VALUES(?,?)',(in_course,in_strength))
 		func.create_course(in_course,in_strength,checkdb.c)
-		func.take_attendance(in_strength)
+		func.take_attendance(in_strength,in_course,checkdb.c)
 
 
 if __name__ == "__main__" : main()
+
--- func.py
+++ func.py
@@ -3,23 +3,48 @@
 import sqlite3
 import datetime
 
-def take_attendance(strength,course,conn_obj): 
+
+def take_attendance(strength,course,conn_obj):
 	today = str(datetime.date.today())
-	conn_obj.execute("ALTER TABLE %s ADD %s char" %(course,today))  
+	print ("Attendance for %s" %today)
+	try:
+		conn_obj.execute("ALTER TABLE %s ADD COLUMN '%s' 'char'" %(course,today))
+	except sqlite3.OperationalError:
+		print "Attendance for the date already taken"
+		return
+	print "Enter p | a | m  for  Present | Absent | Manual_entry" 
 	for x in range(1,strength):
-		print ("%d ." %x)
-		if (raw_input("").lower())=='p':
-			print "present"
-			conn_obj.execute("UPDATE %s SET %s='P' WHERE roll=%d" %(course,today,x))
-		if (raw_input("").lower())=='a':
-			print "absent"
-			conn_obj.execute("UPDATE %s SET %s='A' WHERE roll=%d" %(course,today,x))
+		print ("%d." %x)
+		att = raw_input("").lower()
+		while(1):
+			if  att =='p' :
+				print "present"
+				conn_obj.execute("UPDATE %s SET '%s'='P' WHERE 'roll'=%d" %(course,today,x))
+				break
+			elif  att == 'a' :
+				print "absent"
+				conn_obj.execute("UPDATE %s SET '%s'='A' WHERE 'roll'=%d" %(course,today,x))
+				break
+			elif att == 'm' :
+				man = int(raw_input("Enter the roll call\n"))
+				man_att = raw_input("Enter p | a  for  Present | Absent\n").lower()
+				if man_att == 'p' :
+					print "present"
+					conn_obj.execute("UPDATE %s SET '%s'='P' WHERE 'roll'=%d" %(course,today,man))
+				elif man_att == 'a' :
+					print "absent"
+					conn_obj.execute("UPDATE %s SET '%s'='A' WHERE 'roll'=%d" %(course,today,man))
+				break	
+			else:
+				print "Enter a valid input"
+				continue
 
 def course_exist(course,conn_obj):
 	for x in conn_obj.execute("SELECT name from courses;"):
-		if (x[0].lower())==(course.lower()):
+		if (x[0].lower()) == (course.lower()):
 			return True
 	return False
+
 
 def create_course(course,strength,conn_obj):
 	conn_obj.execute('CREATE TABLE %s(roll int);' %course)
